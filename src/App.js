ed3053ee814876da447e4ddbeffb7d112e71c959
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-11-10 01:26:38 -0700

    edit button working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
 import './App.css';
+import {v4 as uuidv4} from "uuid"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import TodoView from "./components/TodoView"
+
+export default class App extends Component {
+
+state = {
+  todoList: [
+    {
+      id:uuidv4(),
+      todo: "clean clothes",
+      editRayTodo: false
+    },
+    {
+      id:uuidv4(),
+      todo: "fix passport",
+      editRayTodo: false
+    },
+    {
+      id:uuidv4(),
+      todo: "check covid results",
+      editRayTodo: false
+    }
+  ],
+  todoValue: "",
+  errorMessage: false,
+  editTodoValue: ""
 }
 
-export default App;
+inputOnChange = (event) => {
+  this.setState({
+    [event.target.name]: event.target.value,
+    errorMessage: false
+  })
+ 
+}
+
+buttonleOnClick = () => {
+
+  if(this.state.todoValue.length === 0 ){
+    this.setState({
+      errorMessage: true
+    })
+    return
+  }
+
+  let arr = [...this.state.todoList, {id:uuidv4(), todo:this.state.todoValue}]
+
+  this.setState({
+    todoList: arr,
+    todoValue: ""
+  })
+}
+
+appHandleDeleteTodo = (targetID) => {
+  let arr = [...this.state.todoList.filter(({id}) => {
+    return id !== targetID
+  } )]
+
+  this.setState({
+    todoList: arr
+  })
+}
+
+appEditOnClick = (targetID) => {
+  let arr = [...this.state.todoList]
+
+  let newArr = arr.map((item) => {
+    if(targetID === item.id){
+     item.editRayTodo = true;
+    }
+    return item
+    
+  
+    
+
+  })
+  this.setState({
+    todoList: newArr
+  })
+
+  
+  
+}
+
+appEditOnChange = (event) => {
+ this.setState({
+   [event.target.name]: event.target.value
+ })
+
+ console.log(this.state.editTodoValue);
+}
+
+appHandleUpdateButton = (targetID) => {
+  
+  let arr = [...this.state.todoList]
+
+  let newArr = arr.map((item) => {
+    if(targetID === item.id){
+      item.todo = this.state.editTodoValue
+      item.editRayTodo = false
+    }
+    return item
+
+  })
+  console.log(newArr);
+  
+
+  this.setState({
+    todoList: newArr,
+    editTodoValue:""
+
+  })
+}
+
+
+  render() {
+    const {todoList, editTodoValue} = this.state
+    return (
+      <div>
+        {this.state.errorMessage ? <div>Please enter a to do!</div> : null}
+
+        <input onChange={this.inputOnChange} type="text" name="todoValue" value={this.state.todoValue} />
+        <button onClick={this.buttonleOnClick} >Submit</button>
+        <TodoView todoList={todoList} 
+                  appHandleDeleteTodo={this.appHandleDeleteTodo}
+                  appEditOnClick={this.appEditOnClick}
+                  editTodoValue={editTodoValue}
+                  appEditOnChange={this.appEditOnChange}
+                  appHandleUpdateButton={this.appHandleUpdateButton}
+        />
+
+        {todoList.length===0 ? <div> No todos. </div> : null}
+        
+      </div>
+    )
+  }
+}
---
+++ src/components/TodoView.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TodoView = ({todoList, appHandleDeleteTodo, appEditOnClick, editTodoValue, appEditOnChange, appHandleUpdateButton})=> {
+    return (
+        <div>
+            <ul>
+          {todoList.map(({id, todo, editRayTodo}) => {
+            return <li key={id} >
+
+               {editRayTodo ? <input onChange={(event) => appEditOnChange(event)} type ="text" name="editTodoValue" value={editTodoValue} /> : <span>{todo}</span>} 
+          {editRayTodo ? <button onClick={() => appHandleUpdateButton(id)} >Update</button>  :<button onClick={() => appEditOnClick(id)} >Edit</button>}
+          
+                <button onClick={() => appHandleDeleteTodo(id)} >Delete</button>
+                </li>
+          })}
+        </ul>
+            
+        </div>
+    )
+}
+
+TodoView.propTypes = {
+
+}
+
+export default TodoView
